15a94ff3a21d900548839ed4061c970202d71924
Nitish Kumar <nitishsinghjtpr@gmail.com>
2025-09-05 09:13:25 +0530

    string

---
+++ String/script.js
@@ -0,0 +1,10 @@
+let string="Nitish"
+//index no 4 pe kon sa word hai
+let d=string[4];
+console.log(d);
+
+
+let firstName="nitish";
+let lastName="kumar";
+let c=firstName.concat(" "+lastName);
+console.log(c);
