a67b29cd2d0fbc800982b40442e44af54227f14d
Nitish Kumar <nitishsinghjtpr@gmail.com>
2025-09-05 09:26:37 +0530

    bassic array

---
+++ Array/basic/script.js
@@ -0,0 +1,6 @@
+let marks=[12,15,16,17,18,19];
+let ans=console.log(marks[1]);
+console.log(marks);//ye original marks print karega
+marks[1]=5;//yanha pe index number 1 pe change kiya gya hai
+console.log(marks);//ye changed marks ko print karega
+
